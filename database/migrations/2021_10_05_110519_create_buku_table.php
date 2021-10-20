052d3792e7b09ceba424ef82513d4f86d6071788
Muhammad Keenan <mkf450@gmail.com>
2021-10-21 02:48:10 +0700

    hapus on update migration

--- database/migrations/2021_10_05_110519_create_buku_table.php
+++ database/migrations/2021_10_05_110519_create_buku_table.php
@@ -11,7 +11,7 @@ class CreateBukuTable extends Migration
      *
      * @return void
      */
-    public function up() 
+    public function up()
     {
         Schema::enableForeignKeyConstraints();
         Schema::create('buku', function (Blueprint $table) {
@@ -25,7 +25,7 @@ class CreateBukuTable extends Migration
             $table->string('editor');
             $table->string('file_gambar');
             $table->timestamp('tgl_insert')->default(\DB::raw('CURRENT_TIMESTAMP'));
-            $table->timestamp('tgl_update')->default(\DB::raw('CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP'));
+            $table->timestamp('tgl_update')->default(\DB::raw('CURRENT_TIMESTAMP'));
             $table->integer('stok');
             $table->integer('stok_tersedia');
 
